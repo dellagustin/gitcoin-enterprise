963ebb0dd982a33a9b3cd6590f478347e9f00c7a
d050420 <michael.spengler@sap.com>
2020-02-03 15:20:35 +0100

    reading a config file

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs-sync'
+import * as path from 'path'
 
 @Injectable()
 export class AppService {
+
+  private gitHubToken = ''
+
+  public constructor() {
+    // tbd
+  }
+
   public getIssues(): string[] {
     return [];
   }
+
   public getHello(): string {
     return 'Hello World!';
   }
+
+  public getGitHubToken(): string {
+    return fs.readJSON(path.join(__dirname, '../.env.json')).gitHubToken;
+  }
+
 }
