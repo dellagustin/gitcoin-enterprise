46f88d04f4336f2840f445fe165ed05958e9a994
michael-spengler <>
2020-02-04 23:33:50 +0100

    preparing for https

--- server/src/gitcoin-enterprise-server.ts
+++ server/src/gitcoin-enterprise-server.ts
@@ -5,18 +5,35 @@ import * as path from 'path'
 import * as fs from 'fs-sync'
 import * as express from 'express'
 import { LoggerService, ELogLevel } from './logger/logger.service'
+const compression = require('compression')
 
 export const pathToStaticAssets = path.join(__dirname, '../docs')
 export const config = fs.readJSON(path.join(__dirname, '../.env.json'))
 
 async function bootstrap() {
   const logger = new LoggerService()
-  const app = await NestFactory.create(AppModule)
-  app.use(cors('*'));
-  // app.use(compression())
+
+  let app
+
+  if (config.port.indexOf('443') !== -1) {
+
+    const keyFile = config.certificatePrivateKeyFile
+    const certFile = config.certificateFile
+    const privateKey = fs.read(keyFile)
+    const certificate = fs.read(certFile)
+    const credentials = { key: privateKey, cert: certificate }
+    app = await NestFactory.create(AppModule, { httpsOptions: credentials })
+
+  } else {
+    app = await NestFactory.create(AppModule)
+  }
+
+  app.use(cors('*'))
+  app.use(compression())
 
   (app as any).useStaticAssets(pathToStaticAssets)
   await app.listen(config.port)
+
   logger.log(ELogLevel.Info, `app is listening on port: ${config.port}`)
 
   if (config.port === '443') {
