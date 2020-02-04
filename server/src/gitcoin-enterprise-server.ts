fbf1d5beb88fe34ec97eea22fca67e1eb76ffb4b
michael-spengler <>
2020-02-04 23:21:52 +0100

    preparing the server

--- server/src/gitcoin-enterprise-server.ts
+++ server/src/gitcoin-enterprise-server.ts
@@ -4,30 +4,33 @@ import * as cors from 'cors'
 import * as path from 'path'
 import * as fs from 'fs-sync'
 import * as express from 'express'
+import { LoggerService, ELogLevel } from './logger/logger.service'
 
 export const pathToStaticAssets = path.join(__dirname, '../docs')
 export const config = fs.readJSON(path.join(__dirname, '../.env.json'))
 
 async function bootstrap() {
+  const logger = new LoggerService()
   const app = await NestFactory.create(AppModule)
   app.use(cors('*'));
   // app.use(compression())
 
   (app as any).useStaticAssets(pathToStaticAssets)
   await app.listen(config.port)
+  logger.log(ELogLevel.Info, `app is listening on port: ${config.port}`)
 
-  // if (config.port === '443') {
-  //   const httpForwarderAPP = express()
+  if (config.port === '443') {
+    const httpForwarderAPP = express()
 
-  //   httpForwarderAPP.get('*', (req, res) => {
-  //     console.log(req.headers.host)
-  //     const saveHost = req.headers.host.replace('http://', 'https://')
-  //     res.redirect(saveHost)
+    httpForwarderAPP.get('*', (req, res) => {
+      logger.log(ELogLevel.Info, `forwarding an http requst to https`)
+      const saveHost = req.headers.host.replace('http://', 'https://')
+      res.redirect(saveHost)
 
-  //   })
+    })
 
-  //   httpForwarderAPP.listen(80)
-  // }
+    httpForwarderAPP.listen(80)
+  }
 
 }
 bootstrap()
