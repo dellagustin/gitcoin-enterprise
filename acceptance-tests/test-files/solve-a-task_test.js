6a873fa0abbecd3e2fd79c39dd8df75269d95cfc
d050420 <michael.spengler@sap.com>
2020-02-08 21:19:00 +0100

    playing around with acceptance test configs

--- acceptance-tests/test-files/solve-a-task_test.js
+++ acceptance-tests/test-files/solve-a-task_test.js
@@ -1,17 +1,7 @@
-const fs = require("fs-sync");
-const path = require("path");
-const config = fs.readJSON(path.join(__dirname, "../.env.json"));
-
 Feature("Solve a Task");
 
 Scenario("test something", async I => {
   I.amOnPage("/");
-  if (config.mode === "demo") {
-    await I.wait(1); // just for demo reasons making sure people can follow
-  }
   I.click("Solve a Task");
-  if (config.mode === "demo") {
-    await I.wait(1); // just for demo reasons making sure people can follow
-  }
   // I.see("Task Explorer");
 });
--- acceptance-tests/test-files/view-ledger_test.js
+++ acceptance-tests/test-files/view-ledger_test.js
@@ -1,14 +1,7 @@
-const fs = require("fs-sync");
-const path = require("path");
-const config = fs.readJSON(path.join(__dirname, "../.env.json"));
-
 Feature("View Ledger");
 
 Scenario("test something", async I => {
   I.amOnPage("/");
-  if (config.mode === "demo") {
-    await I.wait(1); // just for demo reasons making sure people can follow
-  }
   // I.click(locate("#burgerMenu"));
   // if (config.mode === "demo") {
   //   await I.wait(1); // just for demo reasons making sure people can follow
